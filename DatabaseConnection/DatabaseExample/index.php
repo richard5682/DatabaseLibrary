0dea6d2f03eba31a07ce1c7300312d16f795bb84
richard5682 <78189350+richard5682@users.noreply.github.com>
2021-02-07 15:56:20 +0800

    AJAX update with interface for PHP
    updated AJAXML.js
    added XMLRECIEVER.inc.php
    moved example into folders

--- DatabaseConnection/DatabaseExample/index.php
+++ DatabaseConnection/DatabaseExample/index.php
@@ -1,25 +1,9 @@
 <?php
-	include_once("DatabaseLibrary/CONNECTSERVER.inc.php");
-	$SQL = Construct_SELECTSQL(
-						array("username","password") // <- ARRAY OF THE SELECT COLLUMN
-						,"client" // <- NAME OF THE TABLE
-						,array() // <- WHERE VARIABLE NAME
-						,array()); // <- WHERE VARIABLE VALUES
-	if(ConnectToDefaultServer("test")){
-		echo("SUCCESS CONNECTION TO DATABASE!<br>");
-		echo("SQL QUERY : <strong>".$SQL."</strong><br>");
-		if ($DATA = QUERY_SQL($SQL)){
-			for($i=0;$i<count($DATA);$i++){
-				echo($i." : ".$DATA[$i]['username']."<br>");
-			}
-		}else{
-			echo("IM EMPTY!");
-		}
+
+	include("DatabaseLibrary/XMLRECIEVER.inc.php");
+	function writeData(){
+		echo("<person>");
+			echo("HELLO");
+		echo("</person>");
 	}
-	
 ?>
-
-
-<html>
-	
-</html>
--- DatabaseConnection/DatabaseLibrary/AJAXML.js
+++ DatabaseConnection/DatabaseLibrary/AJAXML.js
@@ -1,29 +1,66 @@
-
-function AJAXconnection(url,id){
+//OBJECT FOR CONNECTION ON PHP
+//url - url for the PHP
+//id - array of string of elements id
+//XMLrecieve - function that will be call when a connection is recieving data
+function AJAXConnection(url,id,XMLRecieve){
 	this.xmlConnection = getXMLConnection();
+	//ELEMENT ID THAT WILL GET THE VALUE
 	this.id = id;
+	//URL OF THE PHP
 	this.url = url;
-	this.xmlConnection.onreadystatechange = stateChange;
+	this.XML;
+	this.xmlConnection.onreadystatechange = XMLRecieve;
 	this.send = processXML;
 }
+
+//GET TABLE OF THE XMLDATA
+//data - data bundle that you want the extraction to happen
+//index - the index where the data is
+//return a data
+function getData(data,index){
+	return data.getElementsByTagName(index);
+}
+
+//GET VALUE OF THE TABLE OF XMLDATA
+//data - data where you want the extraction to happen
+//index - the index where that data is
+//return a value
+function getValue(data, index){
+	return data[index].childNodes[0].nodeValue;
+}
+
+//SEND FUNCTION
+//WILL SEND A REQUEST TO THE PHP IN GET FORMAT
 function processXML(){
 	if(this.xmlConnection.readyState == 4 || this.xmlConnection.readyState == 0){
-		console.log(constructUrl(this.url,this.id));
+		this.xmlConnection.open('GET',constructUrl(this.url,this.id),true);
+		this.xmlConnection.send();
 	}
 }
+
+//CONSTRUCT THE URL IN GET METHOD FORM
 function constructUrl(url,id){
-	var buffer;
-	for(i=0;i<count(id);i++){
+	var buffer="";
+	for(i=0;i<(id.length);i++){
 		if(i!=0){
-			buffer += ",";
+			buffer += "&";
 		}
-		buffer += id[i]+"="+document.getElementById[id[i]].value;
+		buffer += id[i]+"="+document.getElementById(id[i]).value;
 	}
-	return url+"?"buffer;
+	return url+"?"+buffer;
 }
-function stateChange(){
-	
+
+//GET THE XMLDATA FROM THE RECIEVER PART OF CONNECTION
+//MUST BE CALLED INSIDE XMLRecieve function this will get the XML response of the PHP
+function getXML(connection,function1){
+	if(connection.xmlConnection.readyState == 4 || connection.xmlConnection.readyState == 200){
+		connection.XML = connection.xmlConnection.responseXML;
+		return true;
+	}
+	return false;
 }
+
+//OBJECT FUNCTION TO GET THE XMLCONNECTION REQUEST
 function getXMLConnection(){
 	return new XMLHttpRequest();
 }
