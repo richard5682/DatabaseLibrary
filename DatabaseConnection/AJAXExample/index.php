1b93b3fe1b1185fe445c18fa54477ea2d115fabf
richard5682 <78189350+richard5682@users.noreply.github.com>
2021-02-07 16:15:46 +0800

    Update index.php
    Update index.php example

--- DatabaseConnection/AJAXExample/index.php
+++ DatabaseConnection/AJAXExample/index.php
@@ -5,20 +5,27 @@
 <html>
 
 	<head>
-		<script type="text/javascript" src="../DatabaseLibrary/AJAXML.js"></script>
+		<script type="text/javascript" src="DatabaseLibrary/AJAXML.js"></script>
 		
-		<script>
-			var url = "XMLRETRIEVER.php";
+		<script type="text/javascript">
+			var url = "EXAMPLE.php";
 			var id = new Array('name','age','gender','key');
-			var conn1 = new AJAXConnection(url,id);
+			var conn1 = new AJAXConnection(url,id,XMLrecieve);
 			function submitform(){
 				conn1.send();
 			}
+			function XMLrecieve(){
+				if(getXML(conn1)){
+					var data = getData(conn1.XML,'person');
+					console.log(getValue(data,0));
+				}
+			}
+			
 		</script>
 	</head>
 	
 	<body>
-		<form onSubmit="javascript: submitform">
+		<form action="javascript: submitform()">
 			<input type="text" id="name">
 			<input type="text" id="age">
 			<input type="text" id="gender">
@@ -26,5 +33,4 @@
 			<input type="submit" value="SUBMIT">
 		</form>
 	</body>
-
 </html>
