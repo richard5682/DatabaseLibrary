e301b4d4725f06bb13edf54d2c39f6a51519a39c
richard5682 <78189350+richard5682@users.noreply.github.com>
2021-02-08 14:43:30 +0800

    Update Utilities.js
    Added dialogue utilities for customization of dialogue box

--- Libraries/JSLIB/Utilities.js
+++ Libraries/JSLIB/Utilities.js
@@ -11,5 +11,28 @@ function CheckInput(inputstring){
 	}else{
 		return false;
 	}
-	
+}
+var init_dialogue = false;
+function showDialogue(header,text,csslink=null){
+	if(!init_dialogue){
+		init_dialogue = true;
+		var link = csslink;
+		if(csslink==null){
+			csslink="../template/dialogbox.css";
+		}
+		var linkelement = document.createElement("link");
+		linkelement.href = csslink;
+		linkelement.rel = "stylesheet";
+		linkelement.type = "text/css";
+		var dialogue="<div onclick=\"javascript: hideDialogue()\" id=\"C_dialogbox\"><h1 id=\"C_dialogboxh1\">"+header+"</h1><p id=\"C_dialogboxp\">"+text+"</p></div>";
+		document.head.appendChild(linkelement);
+		document.body.innerHTML += dialogue;
+	}else{
+		document.getElementById("C_dialogboxh1").innerHTML = header;
+		document.getElementById("C_dialogboxp").innerHTML = text;
+		document.getElementById("C_dialogbox").style.display="block";
+	}
+}
+function hideDialogue(){
+	document.getElementById("C_dialogbox").style.display="none";
 }
